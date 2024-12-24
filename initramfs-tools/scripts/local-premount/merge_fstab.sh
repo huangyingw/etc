25b59e057e109d30486c67ef4af0ebd632562f47
Ying Huang <huangyingw@gmail.com>
2024-12-24 14:00:33 -0800

    n

--- initramfs-tools/scripts/local-premount/merge_fstab.sh
+++ initramfs-tools/scripts/local-premount/merge_fstab.sh
@@ -10,6 +10,11 @@ fi
 # Merge fstab.common and fstab.local into fstab
 cat /etc/fstab.local /etc/fstab.common > /etc/fstab
 
+# Create mount points from fstab
+awk '$2 ~ /^\// {print $2}' /etc/fstab | while read mountpoint; do
+    mkdir -p "$mountpoint"
+done
+
 # Log the merge process
 echo "Merged fstab at $(date)" >> /var/log/merge_fstab.log
 mount -a
